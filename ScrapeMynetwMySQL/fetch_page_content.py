5f8989a498969f7a421c0f7045d88e8100437833
Aydın Emir Solak <aydinemirsolak@gmail.com>
2024-08-07 00:03:48 +0300

    Update fetch_page_content.py

---
+++ ScrapeMynetwMySQL/fetch_page_content.py
@@ -0,0 +1,9 @@
+import requests
+
+def fetch_page_content(url):
+    response = requests.get(url)
+    if response.status_code == 200:
+        return response.content
+    else:
+        print(f"Error: {response.status_code}")
+        return None
